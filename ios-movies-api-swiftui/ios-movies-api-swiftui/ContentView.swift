be961678b22f4cf55537fbf560c763054101bcfc
Matteo Pasotti <matteopasotti1@gmail.com>
2020-05-21 21:24:04 +0100

    first commit

---
+++ ios-movies-api-swiftui/ios-movies-api-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-movies-api-swiftui
+//
+//  Created by Matteo Pasotti on 21/05/2020.
+//  Copyright © 2020 Matteo Pasotti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
